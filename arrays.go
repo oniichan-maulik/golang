c7f7bc50fe29c3846010c66534561f74fe4db154
oniichan-maulik <139888689+oniichan-maulik@users.noreply.github.com>
2026-09-15 16:48:41 +0530

    Add files via upload

---
+++ arrays.go
@@ -0,0 +1,29 @@
+package main
+
+func main() {
+	// zero values
+
+	// var nums [4]int
+	// nums[0] = 1
+	// fmt.Println(nums)
+
+	// array length
+	// fmt.Println(len(nums))
+
+	// var vals [4]bool
+	// vals[2] = true
+	// fmt.Println(vals)
+
+	// var name [3]string
+	// name[0] = "hi"
+	// fmt.Println(name)
+
+	// single line declaration
+	// nums := [3]int{1, 2, 3}
+	// fmt.Println(nums)
+
+	// 2d arrays
+	// nums := [2][2]int{{1, 2}, {5, 6}}
+	// fmt.Println(nums)
+
+}
