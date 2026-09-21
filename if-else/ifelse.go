f4917cd273daa754fa1cf3ddc154db48740254e1
oniichan-maulik <139888689+oniichan-maulik@users.noreply.github.com>
2026-09-21 23:38:15 +0530

    Add files via upload

---
+++ if-else/ifelse.go
@@ -0,0 +1,28 @@
+package main
+
+func main() {
+	// age1 := 10
+	// if age > 16 {
+	// 	fmt.Println("he is an adult")
+	// } else {
+	// 	fmt.Println("he is a teenager")
+	// }
+	// if age >= 18 {
+	// 	fmt.Println("He is an adult")
+	// } else if age >= 12 {
+	// 	fmt.Println("He is an teenager")
+	// } else {
+	// 	fmt.Println("He is a kid")
+	// }
+	// role := "admin"
+	// Permission := false
+
+	// if role == "admin" && Permission {
+	// 	fmt.Println("yes")
+	// }
+	// if age := 20; age >= 18 {
+	// 	fmt.Println("Person is an adult")
+	// } else if age >= 12 {
+	// 	fmt.Println("He is teenager")
+	// }
+}
